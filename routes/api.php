8466b43e9a57f524a6d4a7d4c5af498092848bf8
faishallathif <faishallathif17@gmail.com>
2022-04-09 13:33:22 +0700

    update middleware

--- routes/api.php
+++ routes/api.php
@@ -24,14 +24,12 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 Route::group(["middleware" => ["jwt.verify"]], function () {
-    Route::resource('kamar', KamarController::class);
+    Route::resource('pesanan', PesananController::class);
+    Route::get("pesanan/cetak/{id}",[PesananController::class,"cetak_pdf"]);
 });
-Route::resource('user', UserController::class);
+Route::resource('kamar', KamarController::class);
 Route::resource('detail-kamar', DetailKamarController::class);
 Route::resource('fasilitas', FasilitasController::class);
-Route::resource('pesanan', PesananController::class);
-Route::get("pesanan/cetak/{id}",[PesananController::class,"cetak_pdf"]);
-
-
+Route::resource('user', UserController::class);
 Route::post('login', [UserController::class, "login"]);
 Route::get('cek_login', [UserController::class, "getAuthenticatedUser"]);
